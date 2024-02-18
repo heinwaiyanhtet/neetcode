1ea0278468c5e70a2a83c8d9a13d18669d011876
Hein Wai Yan Htet <heinwaiyanhtet2020@gmail.com>
2024-02-18 12:58:42 +0630

    setup

---
+++ binarysearch.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public int Search(int[] nums, int target) {
+        
+        // Console.WriteLine(1);
+        return 1;
+
+    }
+}
+
+Solution solution = new();
+solution.Search([1,2,3],2);
