78dd903cab07ff800d7ee5768897966c1400c3fe
Hein Wai Yan Htet <heinwaiyanhtet2020@gmail.com>
2024-02-18 13:32:21 +0630

    binary search

--- binarysearch.cs
+++ binarysearch.cs
@@ -1,11 +1,29 @@
 public class Solution {
-    public int Search(int[] nums, int target) {
-        
-        // Console.WriteLine(1);
-        return 1;
 
+    public int BinarySearch(int[] nums, int target) {
+
+        int left = 0;
+        int right = nums.Length - 1;
+
+        while (left <= right)
+        {
+            int mid = left + (right - left) / 2;
+            if(nums[mid] == target)
+            {   
+                Console.WriteLine(mid);
+                return mid; 
+            }else if(nums[mid] < target){
+                left =  nums[mid] + 1; 
+            }
+            else if(nums[mid] > target){
+                right =  nums[mid] - 1;
+            }
+        }
+
+        return -1;
     }
 }
 
 Solution solution = new();
-solution.Search([1,2,3],2);
+solution.BinarySearch([-1,0,3,5,9,12],9);
+
