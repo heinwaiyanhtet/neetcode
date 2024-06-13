3492011002e5cbc7dbab47c8a87f1432d77f7902
Hein Wai Yan Htet <heinwaiyanhtet2020@gmail.com>
2024-06-13 23:03:12 +0630

    stack

---
+++ Stack/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+//  Valid Parentheses
+
+using System.Collections;
+
+public class Solution
+{
+    public bool isValid(string words)
+    {
+
+        Stack myStack = new Stack(); 
+
+        foreach (var item in words)
+        {
+            Console.WriteLine(item);
+        }
+        
+        return true;
+
+    }
+}
+
+
+public class Program{
+
+
+    public static void Main(string[] args)
+    {
+
+        Solution solution = new Solution();
+
+        Console.WriteLine(solution.isValid("()"));
+        Console.WriteLine(solution.isValid("()[]{}"));
+        Console.WriteLine(solution.isValid("(]"));
+
+    }
+}
