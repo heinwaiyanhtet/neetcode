61107fbc2967a74d0eb7054ae2e412357db66155
Hein Wai Yan Htet <heinwaiyanhtet2020@gmail.com>
2024-09-08 15:17:31 +0630

    linkedlist pracitse start

--- Stack/Program.cs
+++ Stack/Program.cs
@@ -1,76 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-// Console.WriteLine("Hello, World!");
-
-
-
-
-
-
-//  Valid Parentheses
-using System.Collections;
-
-public class Solution
+﻿namespace MainProgram
 {
-    public bool isValid(string words)
+    class Program  
     {
-
-        Stack myStack = new Stack(); 
-
-        foreach (char word in words)
+        static void Main(string[] args)
         {
-            if(word == '(' || word == '[' || word == '{')
-            {
-                myStack.Push(word);
-            }
-
-            else
-            {
-
-                if(myStack.Count == 0)
-                {
-                    return false;
-                }    
-
-                char top = (char)myStack.Pop();
-
-                if(
-                    word == ')' && top != '(' ||
-                    word == ']' && top != '[' ||
-                    word == '}' && top != '{' 
-                )
-                {
-                    return false;
-                }
-
-
-                return true;
-            }
+            
         }
 
-
-
-        Console.WriteLine(myStack.Count); 
-
-        return true;
-
     }
 }
-
-
-public class Program{
-
-
-    public static void Main(string[] args)
-    {
-
-        Solution solution = new Solution();
-
-        Console.WriteLine(solution.isValid("()"));
-        Console.WriteLine(solution.isValid("()[]{}"));
-        Console.WriteLine(solution.isValid("(]"));
-
-    }
-}
-
-
-//Min Stack
