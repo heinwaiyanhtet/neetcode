40f4e56ba33dbb87e8b66b773f1252c20a4bcb67
Hein Wai Yan Htet <heinwaiyanhtet2020@gmail.com>
2024-09-10 02:05:52 +0630

    maximum numbers

---
+++ maxnumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace MaxProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxValue = FindMaxValue(new int[] { 1, 2, 3, 4, 5 });
+
+            Console.WriteLine(maxValue);
+        }
+
+        static int FindMaxValue(int[] numbers)
+        {
+            int maxValue = int.MinValue;
+
+            foreach (var item in numbers)
+            {
+                if (item > maxValue)
+                {
+                    maxValue = item;
+                }
+            }
+
+            return maxValue;
+        }
+    }   
+}
