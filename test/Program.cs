d5e40a95370263bee5166dbc63fa7a5c8979a677
Hein Wai Yan Htet <heinwaiyanhtet2020@gmail.com>
2024-10-17 12:34:53 +0630

    code

---
+++ test/Program.cs
@@ -0,0 +1,36 @@
+﻿class Program
+{
+
+    static void AddTen(ref int number)
+    {
+        number += 10;
+    }
+
+    static void GetValues(out int number1,out int number2)
+    {
+        number1 = 10;
+        number2 = 20;
+    }
+
+    static void Main(string[] args)
+    {
+
+            int myNumber = 5;
+            Console.WriteLine("Before calling AddTen: " + myNumber);
+
+            AddTen(ref myNumber);
+
+            Console.WriteLine($"After ref method: {myNumber}");  
+
+
+
+
+            int a , b;
+
+            GetValues(out a, out b);
+
+            Console.WriteLine($"Out method values: a = {a}, b = {b}");  
+
+            Console.ReadLine();  
+    }
+}
